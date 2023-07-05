6311a59aac7066f2624724312832ca91bcbfe0fd
Nazlı Özçubukçuoğlu <53702068+Greyfourt@users.noreply.github.com>
2023-07-05 17:22:52 +0200

    Rename Tutorial to Documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Documentation',
           },
           {
             href: 'https://hub.docker.com/r/codenotary',
